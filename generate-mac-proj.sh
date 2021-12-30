c8c473ecbcc06a4f93df9ae7ffd62270aecb5407
Sheen Tian <sheentianshen@gmail.com>
2021-12-30 15:10:25 +0800

    Reform generator scripts

--- generate-mac-proj.sh
+++ generate-mac-proj.sh
@@ -1,1 +1,21 @@
-cmake -G "Xcode" -S . -B .build.mac -DBUILD_DEMO=ON -DUSE_SANDBOX=ON
+#!/bin/bash
+
+BUILD_PROJECT=0
+
+BUILD_DIR="$(pwd)/.build/linux"
+
+while getopts bi flag
+do
+    case "${flag}" in
+        b) BUILD_PROJECT=1;;
+    esac
+done
+
+echo ============== Config project ==============
+cmake -G "Xcode" -S . -B "${BUILD_DIR}" -DBUILD_DEMO=ON -DUSE_SANDBOX=ON
+
+if [ ${BUILD_PROJECT} -eq 1 ] 
+then
+    echo ============== Build project ==============
+    cmake --build "${BUILD_DIR}"
+fi
--- generate-win-proj.bat
+++ generate-win-proj.bat
@@ -1,1 +1,1 @@
-cmake -S . -B .build.win -DBUILD_DEMO=ON
+cmake -S . -B .build/Windows -DBUILD_DEMO=ON
