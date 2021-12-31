cd91650408ec5966e490c06580383c9f8149d8a6
Sheen Tian <sheentianshen@gmail.com>
2021-12-31 20:06:04 +0800

    Update

--- src/mac/details/CCefManager_mac.mm
+++ src/mac/details/CCefManager_mac.mm
@@ -2,63 +2,120 @@
 
 #include "CCefSetting.h"
 
-CCefManager::CCefManager()
-  : bridgeObjectName()
-  , debugPort(0)
-  , backgroundColor(-1)
-  , is_initialized_(false)
-  , is_exiting_(false)
-{}
-
-CCefManager&
-CCefManager::getInstance()
+void
+runMessageLoop()
 {
-  static CCefManager s_instance;
-  return s_instance;
+  CefRunMessageLoop();
+}
+
+void
+exitMessageLoop()
+{
+  CefQuitMessageLoop();
+}
+
+const char*
+cefSubprocessPath()
+{
+  static std::string path;
+  if (!path.empty())
+    return path.c_str();
+
+  @autoreleasepool {
+    NSString* fxPath = [[NSBundle bundleForClass:CocoaCefSetting.class] resourcePath];
+    fxPath = [fxPath stringByAppendingPathComponent:@HELPER_BUNDLE_NAME];
+    fxPath = [fxPath stringByAppendingPathComponent:@"Contents"];
+    fxPath = [fxPath stringByAppendingPathComponent:@"MacOS"];
+    fxPath = [fxPath stringByAppendingPathComponent:@HELPER_BINARY_NAME];
+    path = fxPath.UTF8String;
+  }
+  return path.c_str();
+}
+
+const char*
+cefFrameworkPath()
+{
+  static std::string path;
+  if (!path.empty())
+    return path.c_str();
+
+  @autoreleasepool {
+    NSString* fxPath = [[NSBundle bundleForClass:CocoaCefSetting.class] resourcePath];
+    fxPath = [fxPath stringByAppendingPathComponent:@CEF_FRAMEWORK_NAME];
+    path = fxPath.UTF8String;
+  }
+  return path.c_str();
+}
+
+const char*
+cefLibraryPath()
+{
+  static std::string path;
+  if (!path.empty())
+    return path.c_str();
+
+  path = cefFrameworkPath();
+  path += "/";
+  path += CEF_BINARY_NAME;
+  return path.c_str();
+}
+
+const char*
+appMainBundlePath()
+{
+  static std::string path;
+  if (!path.empty())
+    return path.c_str();
+
+  @autoreleasepool {
+    path = [[[NSBundle mainBundle] bundlePath] UTF8String];
+  }
+  return path.c_str();
+}
+
+bool
+loadCefLibrary()
+{
+  return (1 == cef_load_library(cefLibraryPath()));
+}
+
+void
+freeCefLibrary()
+{
+  cef_unload_library();
 }
 
 bool
 CCefManager::initializeCef(int argc, const char* argv[])
 {
-  std::lock_guard<std::mutex> lock(init_locker_);
-
-  // This is not the first time initialization
-  if (is_initialized_)
-    return true;
-
-#if defined(OS_MACOS)
   // load the cef library
   if (!loadCefLibrary()) {
     return false;
   }
-#endif
-
-#if defined(OS_WINDOWS)
-  // Enable High-DPI support on Windows 7 or newer.
-  CefEnableHighDPISupport();
-#endif
-
-  // This is the first time initialization
-  CCefSetting::initializeInstance();
 
   // Build CefSettings
   CefSettings cef_settings;
-  CefString(&cef_settings.browser_subprocess_path) = CCefSetting::browser_sub_process_path;
-  CefString(&cef_settings.resources_dir_path) = CCefSetting::resource_directory_path;
-  CefString(&cef_settings.locales_dir_path) = CCefSetting::locales_directory_path;
-  CefString(&cef_settings.user_agent) = CCefSetting::user_agent;
-  CefString(&cef_settings.cache_path) = CCefSetting::cache_path;
-  CefString(&cef_settings.user_data_path) = CCefSetting::user_data_path;
-  CefString(&cef_settings.locale) = CCefSetting::locale;
-  CefString(&cef_settings.accept_language_list) = CCefSetting::accept_language_list;
+  if (!settings.d->browserSubProcessPath_.empty())
+    CefString(&cef_settings.browser_subprocess_path) = settings.d->browserSubProcessPath_;
+    
+  if (!settings.d->resourceDirectoryPath_.empty())
+    CefString(&cef_settings.resources_dir_path) = settings.d->resourceDirectoryPath_;
+  if (!settings.d->localesDirectoryPath_.empty())
+    CefString(&cef_settings.locales_dir_path) = settings.d->localesDirectoryPath_;
+  if (!settings.d->userAgent_.empty())
+    CefString(&cef_settings.user_agent) = settings.d->userAgent_;
+  if (!settings.d->cachePath_.empty())
+    CefString(&cef_settings.cache_path) = settings.d->cachePath_;
+  if (!settings.d->userDataPath_.empty())
+    CefString(&cef_settings.user_data_path) = settings.d->userDataPath_;
+  if (!settings.d->locale_.empty())
+    CefString(&cef_settings.locale) = settings.d->locale_;
+  if (!settings.d->acceptLanguageList_.empty())
+    CefString(&cef_settings.accept_language_list) = settings.d->acceptLanguageList_;
 
-  cef_settings.persist_session_cookies = CCefSetting::persist_session_cookies;
-  cef_settings.persist_user_preferences = CCefSetting::persist_user_preferences;
-  cef_settings.remote_debugging_port = CCefSetting::remote_debugging_port;
-  cef_settings.background_color = CCefSetting::background_color;
-  cef_settings.no_sandbox = true;
-  cef_settings.pack_loading_disabled = false;
-  cef_settings.multi_threaded_message_loop = true;
+  cef_settings.persist_session_cookies = settings.d->persistSessionCookies_;
+  cef_settings.persist_user_preferences = settings.d->persistUserPreferences_;
+  cef_settings.background_color = settings.d->backgroundColor_;
 
 #ifndef NDEBUG
   cef_settings.log_severity = LOGSEVERITY_DEFAULT;
@@ -67,24 +124,20 @@ CCefManager::initializeCef(int argc, const char* argv[])
   cef_settings.log_severity = LOGSEVERITY_DISABLE;
 #endif
 
-  auto app = new CefViewBrowserApp(CCefSetting::bridge_object_name);
-
-#if defined(OS_WINDOWS)
-  HINSTANCE hInstance = ::GetModuleHandle(nullptr);
-  CefMainArgs main_args(hInstance);
-#else
-  CefMainArgs main_args(argc, (char**)argv);
-#endif
+  // fixed values
+  cef_settings.no_sandbox = true;
+  cef_settings.pack_loading_disabled = false;
+  cef_settings.multi_threaded_message_loop = false;
 
   // Initialize CEF.
-  void* sandboxInfo = nullptr;
-  if (!CefInitialize(main_args, cef_settings, app_, sandboxInfo)) {
+  CefMainArgs main_args(argc, argv);
+  auto app = new CefViewBrowserApp(settings.d->bridgeObjectName_.ToString());
+  if (!CefInitialize(main_args, cef_settings, app, nullptr)) {
     assert(0);
     return false;
   }
 
   app_ = app;
-  is_initialized_ = true;
 
   return true;
 }
@@ -92,7 +145,7 @@ CCefManager::initializeCef(int argc, const char* argv[])
 void
 CCefManager::uninitializeCef()
 {
-  if (!isInitialized())
+  if (!app_)
     return;
 
   // Destroy the application
@@ -101,36 +154,7 @@ CCefManager::uninitializeCef()
   // shutdown the cef
   CefShutdown();
 
-#if defined(OS_MACOS)
   freeCefLibrary();
-#endif
-}
-
-bool
-CCefManager::isInitialized()
-{
-  std::lock_guard<std::mutex> lock(init_locker_);
-  return is_initialized_;
-}
-
-bool
-CCefManager::addCookie(const std::string& name,
-                       const std::string& value,
-                       const std::string& domain,
-                       const std::string& url)
-{
-  CefCookie cookie;
-  CefString(&cookie.name).FromString(name);
-  CefString(&cookie.value).FromString(value);
-  CefString(&cookie.domain).FromString(domain);
-  return CefCookieManager::GetGlobalManager(nullptr)->SetCookie(CefString(url), cookie, nullptr);
-}
-
-void
-CCefManager::registerBrowserHandler(CefRefPtr<CefViewBrowserHandler> handler)
-{
-  std::lock_guard<std::mutex> lock(handler_set_locker_);
-  handler_set_.insert(handler);
 }
 
 void
@@ -160,87 +184,4 @@ CCefManager::closeAllBrowserHandler()
     NSView* view = (__bridge NSView*)(handler->GetBrowser()->GetHost()->GetWindowHandle());
     [view removeFromSuperview];
   }
-}
-
-void
-CCefManager::runMessageLoop()
-{
-  CefRunMessageLoop();
-}
-
-void
-CCefManager::exitMessageLoop()
-{
-  CefQuitMessageLoop();
-}
-
-const char*
-CCefManager::cefSubprocessPath()
-{
-  static std::string path;
-  if (!path.empty())
-    return path.c_str();
-
-  @autoreleasepool {
-    NSString* fxPath = [[NSBundle bundleForClass:CocoaCefSetting.class] resourcePath];
-    fxPath = [fxPath stringByAppendingPathComponent:@HELPER_BUNDLE_NAME];
-    fxPath = [fxPath stringByAppendingPathComponent:@"Contents"];
-    fxPath = [fxPath stringByAppendingPathComponent:@"MacOS"];
-    fxPath = [fxPath stringByAppendingPathComponent:@HELPER_BINARY_NAME];
-    path = fxPath.UTF8String;
-  }
-  return path.c_str();
-}
-
-const char*
-CCefManager::cefFrameworkPath()
-{
-  static std::string path;
-  if (!path.empty())
-    return path.c_str();
-
-  @autoreleasepool {
-    NSString* fxPath = [[NSBundle bundleForClass:CocoaCefSetting.class] resourcePath];
-    fxPath = [fxPath stringByAppendingPathComponent:@CEF_FRAMEWORK_NAME];
-    path = fxPath.UTF8String;
-  }
-  return path.c_str();
-}
-
-const char*
-CCefManager::cefLibraryPath()
-{
-  static std::string path;
-  if (!path.empty())
-    return path.c_str();
-
-  path = cefFrameworkPath();
-  path += "/";
-  path += CEF_BINARY_NAME;
-  return path.c_str();
-}
-
-const char*
-CCefManager::appMainBundlePath()
-{
-  static std::string path;
-  if (!path.empty())
-    return path.c_str();
-
-  @autoreleasepool {
-    path = [[[NSBundle mainBundle] bundlePath] UTF8String];
-  }
-  return path.c_str();
-}
-
-bool
-CCefManager::loadCefLibrary()
-{
-  return (1 == cef_load_library(cefLibraryPath()));
-}
-
-void
-CCefManager::freeCefLibrary()
-{
-  cef_unload_library();
 }
