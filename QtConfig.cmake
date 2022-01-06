f51e98d94bf66419f439258cfa47f3982a3cb05d
Sheen Tian <sheentianshen@gmail.com>
2022-01-06 16:36:50 +0800

    Update Qt SDK dir for appveyor build

--- QtConfig.cmake
+++ QtConfig.cmake
@@ -11,8 +11,22 @@ set(QT_SDK_DIR
 # For CI system
 #
 if (DEFINED ENV{APPVEYOR})
-  set(QT_SDK_DIR
-    # Change this value to the Qt SDK path of your build environment
-    "C:\\Qt\\5.15.2\\msvc2019_64"
-  )
+  if(OS_WINDOWS)
+    set(QT_SDK_DIR
+      # Change this value to the Qt SDK path of your build environment
+      "C:\\Qt\\5.15.2\\msvc2019_64"
+    )
+  elseif (OS_LINUX)
+    set(QT_SDK_DIR
+      # Change this value to the Qt SDK path of your build environment
+      "$HOME/Qt/6.2"
+    )
+  elseif(OS_MACOS)
+    set(QT_SDK_DIR
+      # Change this value to the Qt SDK path of your build environment
+      "$HOME/Qt/6.1"
+    )
+  else()
+    message(FATAL_ERROR "Unsupported plaftorm")
+  endif()
 endif()
