1aa45621b19d1eb788e77e152cd057df4f2c81a5
Sheen Tian <sheentianshen@gmail.com>
2022-01-10 18:43:13 +0800

    Fix performance issue

--- src/details/QCefContextPrivate.cpp
+++ src/details/QCefContextPrivate.cpp
@@ -1,4 +1,4 @@
-﻿#include "QCefContextPrivate.h"
+#include "QCefContextPrivate.h"
 
 #pragma region qt_headers
 #include <QThread>
@@ -15,8 +15,7 @@ QCefContextPrivate::QCefContextPrivate()
 {
   QObject::connect(&cefWorkerTimer_, &QTimer::timeout, this, &QCefContextPrivate::performCefLoopWork);
   cefWorkerTimer_.setTimerType(Qt::PreciseTimer);
-  cefWorkerTimer_.setInterval(0); // at startup we want the high priority
-  cefWorkerTimer_.start();
+  cefWorkerTimer_.start(kCefWorkerIntervalMs);
 }
 
 QCefContextPrivate::~QCefContextPrivate() {}
@@ -46,15 +45,7 @@ QCefContextPrivate::scheduleCefLoopWork(int64_t delayMs)
 {
   // calculate the effective delay number
   auto delay = qMax((int64_t)0, qMin(delayMs, kCefWorkerIntervalMs));
-
-  // update timer interval by different thread context
-  if (QThread::currentThread() == this->thread()) {
-    // current it is in main GUI thread, update timer interval directly
-    cefWorkerTimer_.start(delay);
-  } else {
-    // not in main GUI thread, postpone the updating of timer interval
-    QMetaObject::invokeMethod(&cefWorkerTimer_, "start", Qt::QueuedConnection, Q_ARG(int, delay));
-  }
+  QTimer::singleShot(delay, this, SLOT(performCefLoopWork()));
 }
 
 void
