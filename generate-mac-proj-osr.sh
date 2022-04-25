bc3f819897f111e4ad23d2efa80db42c34ca35d0
Sheen Tian <sheentianshen@gmail.com>
2022-04-26 00:13:59 +0800

    Add macOS OSR generator

---
+++ generate-mac-proj-osr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BUILD_PROJECT=0
+
+BUILD_DIR="$(pwd)/.build/macOS"
+
+while getopts bi flag
+do
+    case "${flag}" in
+        b) BUILD_PROJECT=1;;
+    esac
+done
+
+echo ============== Config project ==============
+cmake -G "Xcode" -S . -B "${BUILD_DIR}" -DBUILD_DEMO=ON -DUSE_OSR=ON -DUSE_SANDBOX=ON
+
+if [ ${BUILD_PROJECT} -eq 1 ] 
+then
+    echo ============== Build project ==============
+    cmake --build "${BUILD_DIR}"
+fi
