32f8a7b5422531ca8e11e5b7ef5b6be008ee2566
Sheen Tian <sheentianshen@gmail.com>
2022-04-28 23:04:06 +0800

    Update CCefClientDelegate_KeyboardHandler.cpp

--- src/details/CCefClientDelegate_KeyboardHandler.cpp
+++ src/details/CCefClientDelegate_KeyboardHandler.cpp
@@ -12,32 +12,32 @@ CCefClientDelegate::onPreKeyEvent(CefRefPtr<CefBrowser> browser,
                                   CefEventHandle os_event,
                                   bool* is_keyboard_shortcut)
 {
-  // qDebug() << "CefKeyEvent:" << event.type;
+  qDebug() << "CefKeyEvent:" << event.type;
 #if defined(Q_OS_MACOS)
   if (event.modifiers & EVENTFLAG_COMMAND_DOWN && event.type == KEYEVENT_RAWKEYDOWN) {
-    switch (event.windows_key_code) {
-      case 0x41: // A
+    switch (event.native_key_code) {
+      case 0: // A
         browser->GetFocusedFrame()->SelectAll();
         *is_keyboard_shortcut = true;
         break;
-      case 0x43: // C
-        browser->GetFocusedFrame()->Copy();
+      case 6: // Z
+        browser->GetFocusedFrame()->Undo();
         *is_keyboard_shortcut = true;
         break;
-      case 0x56: // V
-        browser->GetFocusedFrame()->Paste();
-        *is_keyboard_shortcut = true;
-        break;
-      case 0x58: // X
+      case 7: // X
         browser->GetFocusedFrame()->Cut();
         *is_keyboard_shortcut = true;
         break;
-      case 0x59: // Y
-        browser->GetFocusedFrame()->Redo();
+      case 8: // C
+        browser->GetFocusedFrame()->Copy();
         *is_keyboard_shortcut = true;
         break;
-      case 0x5a: // Z
-        browser->GetFocusedFrame()->Undo();
+      case 9: // V
+        browser->GetFocusedFrame()->Paste();
+        *is_keyboard_shortcut = true;
+        break;
+      case 16: // Y
+        browser->GetFocusedFrame()->Redo();
         *is_keyboard_shortcut = true;
         break;
     }
