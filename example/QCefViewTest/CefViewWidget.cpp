9644d5db07f34f32d64a8ab9a31c8d73e11c3516
Sheen Tian <sheentianshen@gmail.com>
2022-06-24 12:03:44 +0800

    Add code to demonstrate customizing CEF window with rounded corners

--- example/QCefViewTest/CefViewWidget.cpp
+++ example/QCefViewTest/CefViewWidget.cpp
@@ -1,53 +1,12 @@
-﻿#include "CefViewWidget.h"
+#include "CefViewWidget.h"
 
 #include <QColor>
 #include <QRandomGenerator>
 #include <QResizeEvent>
-#include <QWindow>
-
-#if defined(Q_OS_WINDOWS)
-#include <windows.h>
-#elif defined(Q_OS_MACOS)
-#else
-#endif
 
 CefViewWidget::CefViewWidget(const QString url, const QCefSetting* setting, QWidget* parent /* = 0*/)
   : QCefView(url, setting, parent)
-{}
+{
+}
 
 CefViewWidget::~CefViewWidget() {}
-
-void
-CefViewWidget::onBrowserWindowCreated(QWindow* win)
-{
-  cefWindow = win;
-
-#if defined(Q_OS_WINDOWS)
-  // initialize the region with rounded-corners for CEF window
-  ::SetWindowRgn(
-    (HWND)(cefWindow->winId()), ::CreateRoundRectRgn(0, 0, size().width(), size().height(), 100, 100), TRUE);
-#elif defined(Q_OS_MACOS)
-
-#else
-
-#endif
-}
-
-void
-CefViewWidget::resizeEvent(QResizeEvent* event)
-{
-  QCefView::resizeEvent(event);
-
-#if defined(Q_OS_WINDOWS)
-  // update the region with rounded-corners for CEF window
-  if (cefWindow) {
-    ::SetWindowRgn((HWND)(cefWindow->winId()),
-                   ::CreateRoundRectRgn(0, 0, event->size().width(), event->size().height(), 100, 100),
-                   TRUE);
-  }
-#elif defined(Q_OS_MACOS)
-
-#else
-
-#endif
-}
