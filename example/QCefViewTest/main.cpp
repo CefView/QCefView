f8716084d162560600088a419b94cf7ddd43c540
Sheen Tian Shen <sheentianshen@gmail.com>
2023-04-16 02:00:26 +0800

    Update example code

--- example/QCefViewTest/main.cpp
+++ example/QCefViewTest/main.cpp
@@ -7,20 +7,28 @@
 int
 main(int argc, char* argv[])
 {
+  // create QApplication instance
   QApplication a(argc, argv);
 
   // build QCefConfig
   QCefConfig config;
+  // set user agent
   config.setUserAgent("QCefViewTest");
+  // set log level
   config.setLogLevel(QCefConfig::LOGSEVERITY_DEFAULT);
+  // set JSBridge object name (default value is QCefViewClient)
   config.setBridgeObjectName("CallBridge");
+  // port for remote debugging (default is 0 and means to disable remote debugging)
   config.setRemoteDebuggingPort(9000);
+  // set background color for all browsers
+  // (QCefSetting.setBackgroundColor will overwrite this value for specified browser instance)
   config.setBackgroundColor(Qt::lightGray);
 
-  // WindowlessRenderingEnabled is set to true by default, set to false to disable the OSR mode
-  // config.setWindowlessRenderingEnabled(false);
+  // WindowlessRenderingEnabled is set to true by default,
+  // set to false to disable the OSR mode
+  config.setWindowlessRenderingEnabled(true);
 
-  // add command line args
+  // add command line args, you can any cef supported switches or parameters
   config.addCommandLineSwitch("use-mock-keychain");
   // config.addCommandLineSwitch("disable-gpu");
   // config.addCommandLineSwitch("enable-media-stream");
@@ -31,11 +39,14 @@ main(int argc, char* argv[])
   config.addCommandLineSwitchWithValue("renderer-process-limit", "1");
   // config.addCommandLineSwitchWithValue("disable-features", "BlinkGenPropertyTrees,TranslateUI,site-per-process");
 
-  // initialize QCefContext instance with config
+  // create QCefContext instance with config,
+  // the lifecycle of cefContext must be the same as QApplication instance
   QCefContext cefContext(&a, argc, argv, &config);
 
+  // application window
   MainWindow w;
   w.show();
 
+  // flying
   return a.exec();
 }
