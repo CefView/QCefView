7bd2824573145d0281ad5a311d597dc968e98e9e
Sheen Tian Shen <sheentianshen@gmail.com>
2023-08-30 20:51:24 +0800

    Remove redundant code

--- src/details/QCefWindow.cpp
+++ src/details/QCefWindow.cpp
@@ -38,32 +38,10 @@ QCefWindow::cefWindow()
 }
 
 void
-QCefWindow::exposeEvent(QExposeEvent* e)
-{
-  //
-  QWindow::exposeEvent(e);
-}
-
-void
 QCefWindow::resizeEvent(QResizeEvent* e)
 {
-  //
   if (cefWindow_) {
     cefWindow_->resize(e->size());
   }
   QWindow::resizeEvent(e);
-}
-
-void
-QCefWindow::showEvent(QShowEvent* e)
-{
-  //
-  QWindow::showEvent(e);
-}
-
-void
-QCefWindow::hideEvent(QHideEvent* e)
-{
-  //
-  QWindow::hideEvent(e);
 }
