456228f810c4f88d54bcbcd8ed37d62f70c14495
Sheen Tian <5869938+tishion@users.noreply.github.com>
2025-03-22 10:30:27 +0800

    feat: workaround to resolve the silent focus release issue

--- src/details/handler/CCefClientDelegate_LoadHandler.cpp
+++ src/details/handler/CCefClientDelegate_LoadHandler.cpp
@@ -30,6 +30,20 @@ CCefClientDelegate::loadEnd(CefRefPtr<CefBrowser>& browser, CefRefPtr<CefFrame>&
   if (!IsValidBrowser(browser))
     return;
 
+  // workaround for:
+  // https://github.com/chromiumembedded/cef/issues/3870
+  // after navigation CEF resets the browser focus status
+  // without any callback notification (AKA, released the
+  // focus silently), so we need to update the CEF browser
+  // focus status according to the one we have kept
+  if (true                                     //
+      && pCefViewPrivate_->isOSRModeEnabled_   //
+      && pCefViewPrivate_->osr.hasCefGotFocus_ //
+      && browser->GetHost()                    //
+  ) {
+    browser->GetHost()->SetFocus(true);
+  }
+
   emit pCefViewPrivate_->q_ptr->loadEnd(
     browser->GetIdentifier(), ValueConvertor::FrameIdC2Q(frame->GetIdentifier()), frame->IsMain(), httpStatusCode);
 }
