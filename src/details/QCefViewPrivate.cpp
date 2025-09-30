c259525c3708aede3303cbe0808568e4cf1ab7f6
oufengfang <oufengfang@163.com>
2025-09-30 13:46:26 +0800

    //Because Chrome OSR needs the IME's pre-edit to get the cursor position for the candidate panel,       //and some IMEs (e.g., Sogou) lack this feature, we simulate it here using the current input.

--- src/details/QCefViewPrivate.cpp
+++ src/details/QCefViewPrivate.cpp
@@ -960,7 +960,7 @@ QCefViewPrivate::onViewInputMethodQuery(Qt::InputMethodQuery query) const
 void
 QCefViewPrivate::onViewInputMethodEvent(QInputMethodEvent* event)
 {
-  if (isOSRModeEnabled_) {
+    if (isOSRModeEnabled_) {
     // OSR mode
     if (!pCefBrowser_)
       return;
@@ -969,30 +969,12 @@ QCefViewPrivate::onViewInputMethodEvent(QInputMethodEvent* event)
     auto composedText = event->commitString();
 
     if (!composedText.isEmpty()) {
+      //Because Chrome OSR needs the IME's pre-edit to get the cursor position for the candidate panel,
+      //and some IMEs (e.g., Sogou) lack this feature, we simulate it here using the current input.
+      this->setImeComposition(event->attributes(), composedText);
       pCefBrowser_->GetHost()->ImeCommitText(composedText.toStdString(), CefRange(UINT32_MAX, UINT32_MAX), 0);
     } else if (!composingText.isEmpty()) {
-      CefCompositionUnderline underline;
-      underline.background_color = 0;
-      underline.range = { 0, static_cast<decltype(CefRange::to)>(composingText.length()) };
-
-      CefRange selectionRange;
-      for (auto& attr : event->attributes()) {
-        switch (attr.type) {
-          case QInputMethodEvent::TextFormat:
-            break;
-          case QInputMethodEvent::Cursor:
-            selectionRange.Set(attr.start, attr.start);
-            break;
-          case QInputMethodEvent::Language:
-          case QInputMethodEvent::Ruby:
-          case QInputMethodEvent::Selection:
-            break;
-          default:
-            break;
-        }
-      }
-      pCefBrowser_->GetHost()->ImeSetComposition(
-        composingText.toStdString(), { underline }, CefRange(UINT32_MAX, UINT32_MAX), selectionRange);
+      this->setImeComposition(event->attributes(), composingText);
     } else {
       pCefBrowser_->GetHost()->ImeCancelComposition();
     }
@@ -1677,4 +1659,31 @@ QCefViewPrivate::zoomLevel()
   }
 
   return 0;
+}
+
+void
+QCefViewPrivate::setImeComposition(const QList<QInputMethodEvent::Attribute>& attributes, const QString& composingText)
+{
+  CefCompositionUnderline underline;
+  underline.background_color = 0;
+  underline.range = { 0, static_cast<decltype(CefRange::to)>(composingText.length()) };
+
+  CefRange selectionRange;
+  for (auto& attr : attributes) {
+    switch (attr.type) {
+      case QInputMethodEvent::TextFormat:
+        break;
+      case QInputMethodEvent::Cursor:
+        selectionRange.Set(attr.start, attr.start);
+        break;
+      case QInputMethodEvent::Language:
+      case QInputMethodEvent::Ruby:
+      case QInputMethodEvent::Selection:
+        break;
+      default:
+        break;
+    }
+  }
+  pCefBrowser_->GetHost()->ImeSetComposition(
+    composingText.toStdString(), { underline }, CefRange(UINT32_MAX, UINT32_MAX), selectionRange);
 }
